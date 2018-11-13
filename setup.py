ad38f01764b93140feb943680e5212c9d01c3fa7
Zoltán Sylvester <zoltan.sylvester@gmail.com>
2018-11-13 14:54:33 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('_dpcore_py',
-                    sources = ['dpcore_py.c'])
+                    sources = ['dpcore_py.c'],
+                   include_dirs = [numpy.get_include()])
 
 setup (name = '_dpcore_py',
        version = '0.0',
